39360cf5379e2d5dc69827a0504de4f56846369f
Your Name <shahanricky86@gmail.com>
2023-04-30 05:39:34 +0600

    news

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require("cors")
-const catagories = require("./data/catagory.json")
+const catagories = require("./data/catagory.json");
+const news = require("./data/news.json")
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -11,6 +12,27 @@ app.get('/', (req, res) => {
 
 app.get('/catagories', (req, res) => {
   res.send(catagories)
+})
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
+})
+
+app.get("/catagories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == 0) {
+    res.send(news)
+  }
+  else {
+    const catagoriesNews = news.filter(n => n.category_id === id);
+    res.send(catagoriesNews)
+  }
 })
 
 app.listen(port, () => {
